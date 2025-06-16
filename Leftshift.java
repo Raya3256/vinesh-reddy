fae637f636a0263f4722a9722c841b85baafdb1a
Raya3256 <vineshreddy2004@gmail.com>
2025-06-16 10:01:18 +0530

    Add files via upload
    It is an java programme that left shift the elements in the given array by K times . it is the optimal solution using reverse function . It take less time and space complexity compared to other method .

---
+++ Leftshift.java
@@ -0,0 +1,33 @@
+public class Leftshift
+{
+    public static void reverse(int arr[],int start, int end)
+    {
+        while(start<=end)
+        {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        
+    }
+        public static void Rotatetoleft(int arr[],int k,int n)
+        {
+            reverse(arr,0,k-1);
+            reverse(arr,k,n-1);
+            reverse(arr,0,n-1);
+            for(int i=0;i<n;i++)
+            {
+                System.out.print(arr[i]+" ");
+            }
+        }
+        public static void main(String args[])
+        {
+            int arr[] = {1,2,3,4,5,6,7};
+            int k = 2;
+            int n = arr.length;
+            Rotatetoleft(arr,k,n);
+        }
+    
+}
